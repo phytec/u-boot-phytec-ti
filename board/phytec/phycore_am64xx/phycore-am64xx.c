15f0de551506c07e31c678ad4bd713452f6eb5fc
Wadim Egorov <w.egorov@phytec.de>
2021-09-27 15:41:58 -0700

    board: phytec: am64x: Add PHYTEC phyCORE-AM64x SoM
    Add support for PHYTEC phyCORE-AM64x SoM.
    
    Supported features:
    - 2GB DDR4 RAM
    - eMMC
    - external SD
    - debug UART
    
    Signed-off-by: Wadim Egorov <w.egorov@phytec.de>

---
+++ board/phytec/phycore_am64xx/phycore-am64xx.c
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2021 PHYTEC America, LLC - https://www.phytec.com
+ *
+ * Based on board/ti/am64x/evm.c
+ *
+ */
+
+#include <common.h>
+#include <asm/io.h>
+#include <spl.h>
+#include <asm/arch/hardware.h>
+#include <asm/arch/sys_proto.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int board_init(void)
+{
+	return 0;
+}
+
+int dram_init(void)
+{
+	gd->ram_size = 0x80000000;
+
+	return 0;
+}
+
+int dram_init_banksize(void)
+{
+	/* Bank 0 declares the memory available in the DDR low region */
+	gd->bd->bi_dram[0].start = CONFIG_SYS_SDRAM_BASE;
+	gd->bd->bi_dram[0].size = 0x80000000;
+	gd->ram_size = 0x80000000;
+
+	return 0;
+}
+#ifdef CONFIG_SPL_LOAD_FIT
+int board_fit_config_name_match(const char *name)
+{
+	if (!strcmp(name, "k3-am64xx-phycore-som-r5") || !strcmp(name, "k3-am64xx-phycore-som"))
+		return 0;
+
+	return -1;
+}
+#endif
+
+#ifdef CONFIG_SPL_BOARD_INIT
+#define CTRLMMR_USB0_PHY_CTRL	0x43004008
+#define CORE_VOLTAGE		0x80000000
+
+void spl_board_init(void)
+{
+	u32 val;
+
+	/* Set USB PHY core voltage to 0.85V */
+	val = readl(CTRLMMR_USB0_PHY_CTRL);
+	val &= ~(CORE_VOLTAGE);
+	writel(val, CTRLMMR_USB0_PHY_CTRL);
+}
+#endif
