d58e8dbdf380b7c234d24433d1b8551aba6fcfba
Vignesh Raghavendra <vigneshr@ti.com>
2023-01-26 11:11:47 +0530

    environment: ti: k3_dfu: Add DFU alt_info for OSPI NAND
    Define dfu_alt_info settings for OSPI NAND to enable flashing to OSPI
    NAND flash via USB
    
    Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

--- include/environment/ti/k3_dfu.h
+++ include/environment/ti/k3_dfu.h
@@ -60,4 +60,13 @@
 	"tispl.bin ram 0x80080000 0x200000;" \
 	"u-boot.img ram 0x81000000 0x400000\0" \
 
+#define DFU_ALT_INFO_OSPI_NAND \
+	"dfu_alt_info_ospi_nand=" \
+	"tiboot3.bin raw 0x0 0x080000;" \
+	"tispl.bin raw 0x080000 0x200000;" \
+	"u-boot.img raw 0x280000 0x400000;" \
+	"u-boot-env raw 0x680000 0x040000;" \
+	"rootfs raw 0x2000000 0x5fc0000;" \
+	"phypattern raw 0x7fc0000 0x40000\0"
+
 #endif /* __TI_DFU_H */
