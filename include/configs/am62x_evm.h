fd6ce7442eda23d508d5834bc0be7a5728c64b8c
Nitin Yadav <n-yadav@ti.com>
2023-02-03 11:21:38 +0530

    configs: am62xx_evm: Drop EXTRA_ENV_DFUARGS for R5 SPL
    Drop EXTRA_ENV_DFUARGS from R5 SPL build to save SRAM
    space. There is no need for DFU args at R5 SPL stage.
    
    Signed-off-by: Nitin Yadav <n-yadav@ti.com>

--- include/configs/am62x_evm.h
+++ include/configs/am62x_evm.h
@@ -429,12 +429,16 @@
 
 #endif
 
+#ifdef CONFIG_TARGET_AM625_A53_EVM
 #define EXTRA_ENV_DFUARGS \
 	DFU_ALT_INFO_MMC \
 	DFU_ALT_INFO_EMMC \
 	DFU_ALT_INFO_RAM \
 	DFU_ALT_INFO_OSPI \
 	DFU_ALT_INFO_OSPI_NAND
+#else
+#define EXTRA_ENV_DFUARGS
+#endif
 
 /* Incorporate settings into the U-Boot environment */
 #define CONFIG_EXTRA_ENV_SETTINGS					\
