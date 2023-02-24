2d7090f5c105314dfa560232153cf531a11e5cc7
Wadim Egorov <w.egorov@phytec.de>
2023-02-24 11:49:46 +0100

    board: phytec: phycore_am64x: Detect environment location
    Porvide a way to detect the environment location using env_get_location().
    For SPI boot, store the environment in SPI at a fixed offset (ENV_OFFSET)
    and for SD/eMMC boot store it in on the boot (FAT) partition.
    
    Signed-off-by: Wadim Egorov <w.egorov@phytec.de>

--- board/phytec/phycore_am64x/phycore-am64x.c
+++ board/phytec/phycore_am64x/phycore-am64x.c
@@ -9,6 +9,7 @@
 #include <common.h>
 #include <asm/io.h>
 #include <env.h>
+#include <env_internal.h>
 #include <spl.h>
 #include <asm/arch/hardware.h>
 #include <asm/arch/sys_proto.h>
@@ -79,6 +80,26 @@ int mmc_get_env_dev(void)
 	return CONFIG_SYS_MMC_ENV_DEV;
 }
 #endif
+
+enum env_location env_get_location(enum env_operation op, int prio)
+{
+	u32 boot_device = get_boot_device();
+
+	if (prio)
+		return ENVL_UNKNOWN;
+
+	switch (boot_device) {
+	case BOOT_DEVICE_MMC1:
+	case BOOT_DEVICE_MMC2:
+		if (CONFIG_IS_ENABLED(ENV_IS_IN_FAT))
+			return ENVL_FAT;
+	case BOOT_DEVICE_SPI:
+		if (CONFIG_IS_ENABLED(ENV_IS_IN_SPI_FLASH))
+			return ENVL_SPI_FLASH;
+	default:
+		return ENVL_NOWHERE;
+	};
+}
 
 int board_late_init(void)
 {
