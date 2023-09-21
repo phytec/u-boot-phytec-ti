42d592fdd6270f2d8b91397f73e61f48069eb50f
John Ma <jma@phytec.com>
2023-09-21 09:51:30 -0700

    board: phytec: common: fix som detection linker error
    Fix linker error when deactivating SoM detection via Kconfig.
    
    The function definitions are moved to the .c files and Makefile is
    updated to build the *som_detection.o instead of conditional build.
    
    (based on commit 582d6fa353cc ("board: phytec: common: fix som detection
    linker error"))
    
    Signed-off-by: John Ma <jma@phytec.com>
    Reviewed-by: Daniel Schultz <d.schultz@phytec.de>
    Tested-by: Daniel Schultz <d.schultz@phytec.de>
    Signed-off-by: Wadim Egorov <w.egorov@phytec.de>

--- board/phytec/common/phytec_som_detection.h
+++ board/phytec/common/phytec_som_detection.h
@@ -62,8 +62,6 @@ struct phytec_eeprom_data {
 	} data;
 } __packed;
 
-#if IS_ENABLED(CONFIG_PHYTEC_SOM_DETECTION)
-
 int phytec_eeprom_data_setup_fallback(struct phytec_eeprom_data *data,
 				      int bus_num, int addr,
 				      int addr_fallback);
@@ -77,44 +75,5 @@ char * __maybe_unused phytec_get_opt(struct phytec_eeprom_data *data);
 
 struct extension *phytec_add_extension(const char *name, const char *overlay,
 				       const char *other);
-
-#else
-
-inline int phytec_eeprom_data_setup(struct phytec_eeprom_data *data,
-				    int bus_num, int addr)
-{
-	return PHYTEC_EEPROM_INVAL;
-}
-
-inline int phytec_eeprom_data_setup_fallback(struct phytec_eeprom_data *data,
-					     int bus_num, int addr,
-					     int addr_fallback)
-{
-	return PHYTEC_EEPROM_INVAL;
-}
-
-inline int phytec_eeprom_data_init(struct phytec_eeprom_data *data,
-				   int bus_num, int addr)
-{
-	return PHYTEC_EEPROM_INVAL;
-}
-
-inline void __maybe_unused phytec_print_som_info(struct phytec_eeprom_data *data)
-{
-}
-
-inline char *__maybe_unused phytec_get_opt(struct phytec_eeprom_data *data)
-{
-	return NULL;
-}
-
-inline struct extension *phytec_add_extension(const char *name,
-					      const char *overlay,
-					      const char *other)
-{
-	return NULL;
-}
-
-#endif /* IS_ENABLED(CONFIG_PHYTEC_SOM_DETECTION) */
 
 #endif /* _PHYTEC_SOM_DETECTION_H */
