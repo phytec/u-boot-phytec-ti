phytec/u-boot-phytec-ti

commit a096e43c3758e53d5a380da5074ed0cb15467717

board: phytec: phytec_am62x: Account for DDR size fixups if ECC is enabled
Call into k3-ddrss driver to fixup device tree and resize the available
amount of DDR if ECC is enabled.

A second fixup is required from A53 SPL to take the fixup as done from
R5 SPL and apply it to DT passed to A53 U-boot, which in turn passes
this to the OS.

Inline ECC is disabled in device tree by default.  It can be enabled by
adding memorycontroller node with "ti,ecc-enable" in
k3-am625-r5-phycore-som-2gb.dts.

Based on commit
4c092bb306b0 (board: ti: am64x: Account for DDR size fixups if ECC is
enabled)

Signed-off-by: John Ma <jma@phytec.com>
Reviewed-by: Daniel Schultz <d.schultz@phytec.de>
Signed-off-by: Wadim Egorov <w.egorov@phytec.de>

board/phytec/phycore_am62x/phycore-am62x.c
@@ -31,6 +31,57 @@ int dram_init_banksize(void)
 }
 
 #if defined(CONFIG_SPL_BUILD)
+#if defined(CONFIG_K3_AM64_DDRSS)
+static void fixup_ddr_driver_for_ecc(struct spl_image_info *spl_image)
+{
+	struct udevice *dev;
+	int ret;
+
+	dram_init_banksize();
+
+	ret = uclass_get_device(UCLASS_RAM, 0, &dev);
+	if (ret)
+		panic("Cannot get RAM device for ddr size fixup: %d\n", ret);
+
+	ret = k3_ddrss_ddr_fdt_fixup(dev, spl_image->fdt_addr, gd->bd);
+	if (ret)
+		printf("Error fixing up ddr node for ECC use! %d\n", ret);
+}
+#else
+static void fixup_memory_node(struct spl_image_info *spl_image)
+{
+	u64 start[CONFIG_NR_DRAM_BANKS];
+	u64 size[CONFIG_NR_DRAM_BANKS];
+	int bank;
+	int ret;
+
+	dram_init();
+	dram_init_banksize();
+
+	for (bank = 0; bank < CONFIG_NR_DRAM_BANKS; bank++) {
+		start[bank] =  gd->bd->bi_dram[bank].start;
+		size[bank] = gd->bd->bi_dram[bank].size;
+	}
+
+	/* dram_init functions use SPL fdt, and we must fixup u-boot fdt */
+	ret = fdt_fixup_memory_banks(spl_image->fdt_addr, start, size,
+				     CONFIG_NR_DRAM_BANKS);
+	if (ret)
+		printf("Error fixing up memory node! %d\n", ret);
+}
+#endif
+
+void spl_perform_fixups(struct spl_image_info *spl_image)
+{
+#if defined(CONFIG_K3_AM64_DDRSS)
+	fixup_ddr_driver_for_ecc(spl_image);
+#else
+	fixup_memory_node(spl_image);
+#endif
+}
+#endif
+
+#if defined(CONFIG_SPL_BUILD)
 int do_board_detect(void)
 {
 	return 0;

commit 190155ba52ef2b3e7b0917da17fcd0e5d82685e5

board: phytec: am62x: Fix implicit declaration warnings
Fix implicit declaration warnings for uclass_get_device() &
k3_ddrss_ddr_fdt_fixup() by including necessary header files.

Fixes: a096e43c3758 ("board: phytec: phytec_am62x: Account for DDR size fixups if ECC is enabled")
Signed-off-by: Wadim Egorov <w.egorov@phytec.de>

board/phytec/phycore_am62x/phycore-am62x.c
@@ -14,6 +14,8 @@
 #include <asm/arch/sys_proto.h>
 #include <extension_board.h>
 #include <malloc.h>
+#include <dm/uclass.h>
+#include <k3-ddrss.h>
 
 #include "../common/am62_som_detection.h"
 