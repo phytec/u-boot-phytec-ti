phytec/u-boot-phytec-ti

commit b6cf77e124078a9662bebf24ff4173819144963f

board: phytec: am57x: Change name_overlays to overlays
PHYTEC products usally use "overlays" as variable to define all
overlays which should get applied during boot. TI uses
"name_overlays" instead.

Switch to "overlays" to align this product with other PHYTEC
products.

Signed-off-by: Daniel Schultz <d.schultz@phytec.de>
Signed-off-by: Wadim Egorov <w.egorov@phytec.de>

include/configs/phycore_am57x.h
@@ -107,7 +107,7 @@
 	"get_overlay_mmc=" \
 		"fdt address ${fdtaddr};" \
 		"fdt resize 0x100000;" \
-		"for overlay in $name_overlays;" \
+		"for overlay in $overlays;" \
 		"do;" \
 		"load mmc ${bootpart} ${dtboaddr} ${bootdir}/${overlay};" \
 		"fdt apply ${dtboaddr};" \
@@ -153,7 +153,7 @@
 	"get_overlay_nand=" \
 		"fdt address ${fdtaddr};" \
 		"fdt resize 0x100000;" \
-		"for overlay in $name_overlays;" \
+		"for overlay in $overlays;" \
 		"do;" \
 		"ubifsload ${dtboaddr} ${bootdir}/${overlay};" \
 		"fdt apply ${dtboaddr};" \

commit 62e32356ac492cfe3a77a2d5e8185572a9046de7

include: configs: phycore_am57x: Use custom get_overlaystring script
Amid recent renaming of environment variables by commit b6cf77e12407
("board: phytec: am57x: Change name_overlays to overlays"), we need to
do similar with the TI provided "get_overlaystring" script. By default
it still uses "name_overlays" variable which does not work anymore for
booting FIT images.

Thus provide custom "get_overlaystring" script which loads overlays from
the "overlays" variable as with other PHYTEC products.

Fixes: b6cf77e12407 ("board: phytec: am57x: Change name_overlays to overlays")
Signed-off-by: Primoz Fiser <primoz.fiser@norik.com>
Signed-off-by: Wadim Egorov <w.egorov@phytec.de>

include/configs/phycore_am57x.h
@@ -104,6 +104,11 @@
 		"run netargs; " \
 		"bootz ${loadaddr} - ${fdtaddr}\0" \
 	"board_override=0\0" \
+	"get_overlaystring=" \
+		"for overlay in $overlays;" \
+		"do;" \
+		"setenv overlaystring ${overlaystring}'#'${overlay};" \
+		"done;\0" \
 	"get_overlay_mmc=" \
 		"fdt address ${fdtaddr};" \
 		"fdt resize 0x100000;" \