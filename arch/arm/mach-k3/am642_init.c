50c449c27bf27657bfa15d48f0fe5c0459ee65cd
Andrew Davis <afd@ti.com>
2024-01-29 08:44:30 -0800

    BACKPORT: arm: mach-k3: Add weak do_board_detect() to common file
    This matches how it was done for pre-K3 TI platforms and it allows
    us to move the forward declaration out of sys_proto.h.
    
    It also removes the need for K3_BOARD_DETECT as one is free to simply
    override the weak function in their board files as needed.
    
    Signed-off-by: Andrew Davis <afd@ti.com>
    Reviewed-by: Christian Gmeiner <christian.gmeiner@gmail.com>
    Conflicts:
    	arch/arm/mach-k3/am642_init.c
    	arch/arm/mach-k3/am654_init.c
    	arch/arm/mach-k3/common.h
    	arch/arm/mach-k3/include/mach/sys_proto.h
    	arch/arm/mach-k3/j721e_init.c
    
    Signed-off-by: Wadim Egorov <w.egorov@phytec.de>
    Signed-off-by: Dominik Haller <d.haller@phytec.de>

--- arch/arm/mach-k3/am642_init.c
+++ arch/arm/mach-k3/am642_init.c
@@ -106,8 +106,8 @@ void do_dt_magic(void)
 {
 	int ret, rescan;
 
-	if (IS_ENABLED(CONFIG_TI_I2C_BOARD_DETECT))
-		do_board_detect();
+	/* Perform board detection */
+	do_board_detect();
 
 	/*
 	 * Board detection has been done.
