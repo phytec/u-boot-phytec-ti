be295a23872d73232b010f957815264bfacca7ae
Wadim Egorov <w.egorov@phytec.de>
2024-02-06 07:16:42 +0100

    board: phytec: common: Introduce a method to inject DDR timings deltas
    Introduce fdt_apply_ddrss_timings_patch() to allow board code to
    override DDR settings in the device tree prior to DDRSS driver probing.
    
    Signed-off-by: Wadim Egorov <w.egorov@phytec.de>
    Tested-by: John Ma <jma@phytec.com>

---
+++ board/phytec/common/k3_ddrss_patch.h
@@ -0,0 +1,22 @@
+#ifndef K3_DDRSS_PATCH
+#define K3_DDRSS_PATCH
+
+#include <linux/types.h>
+
+struct ddr_reg {
+	u32 off;
+	u32 val;
+};
+
+struct ddrss {
+	struct ddr_reg *ctl_regs;
+	u32 ctl_regs_num;
+	struct ddr_reg *pi_regs;
+	u32 pi_regs_num;
+	struct ddr_reg *phy_regs;
+	u32 phy_regs_num;
+};
+
+int fdt_apply_ddrss_timings_patch(void *fdt, struct ddrss *ddrss);
+
+#endif /* K3_DDRSS_PATCH */
