c8d3b968254020f3c55c5f28c62ee48f6e8a06db
Nathan Morrisson <nmorrisson@phytec.com>
2024-03-06 14:21:40 -0800

    board: phytec: common: Add OSPI and QSPI check functions
    Add inline functions to check if the board has OSPI NOR or QSPI NOR
    installed.
    
    Signed-off-by: Nathan Morrisson <nmorrisson@phytec.com>
    Signed-off-by: Wadim Egorov <w.egorov@phytec.de>

--- board/phytec/common/am64_som_detection.h
+++ board/phytec/common/am64_som_detection.h
@@ -17,4 +17,17 @@ u8 __maybe_unused phytec_get_am64_spi(struct phytec_eeprom_data *data);
 u8 __maybe_unused phytec_get_am64_eth(struct phytec_eeprom_data *data);
 u8 __maybe_unused phytec_get_am64_rtc(struct phytec_eeprom_data *data);
 
+static inline int phytec_am64_is_qspi(struct phytec_eeprom_data *data)
+{
+	u8 spi = phytec_get_am64_spi(data);
+
+	if (spi == PHYTEC_EEPROM_VALUE_X)
+		return 0;
+	return spi <= PHYTEC_EEPROM_NOR_FLASH_64MB_QSPI;
+}
+
+static inline int phytec_am64_is_ospi(struct phytec_eeprom_data *data)
+{
+	return phytec_get_am64_spi(data) > PHYTEC_EEPROM_NOR_FLASH_64MB_QSPI;
+}
 #endif /* _PHYTEC_AM64_SOM_DETECTION_H */
