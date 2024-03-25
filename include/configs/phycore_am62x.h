c9379751402902cd8846c54f181d5d4331516ad4
Wadim Egorov <w.egorov@phytec.de>
2024-03-25 10:05:32 +0100

    include: configs: phycore_am6*: Cleanup configs
    Remove not relevant includes, sizes.h & ti_armv7_common.h.
    Pull in CFG_SYS_SDRAM_BASE as it was defined in ti_armv7_common.h.
    
    Signed-off-by: Wadim Egorov <w.egorov@phytec.de>

--- include/configs/phycore_am62x.h
+++ include/configs/phycore_am62x.h
@@ -9,12 +9,8 @@
 #ifndef __PHYCORE_AM62X_H
 #define __PHYCORE_AM62X_H
 
-#include <linux/sizes.h>
-
 /* DDR Configuration */
+#define CFG_SYS_SDRAM_BASE		0x80000000
 #define CFG_SYS_SDRAM_BASE1		0x880000000
-
-/* Now for the remaining common defines */
-#include <configs/ti_armv7_common.h>
 
 #endif /* __PHYCORE_AM62X_H */
