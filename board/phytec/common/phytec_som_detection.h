590c55f9494061d2261f4bf9bc2de11d2d42ee78
Primoz Fiser <primoz.fiser@norik.com>
2024-05-27 12:55:49 +0200

    board: phytec: common: Add AM57 EEPROM APIv0 support
    AM57x SOMs shipped before PD20.1.0 releases had specific AM57 EEPROM
    data structure format stored in the their EEPROMs (predates common
    PHYTEC_SOM_DETECTION API revision 0). Allow those SOMs to still boot
    correctly without updating the EEPROM contents. Introduce new Kconfig
    option CONFIG_PHYTEC_AM57_EEPROM_APIV0 and select it by default on AM57
    platforms.
    
    NOTE: SOMs flashed after PD20.1.0 release with common EEPROM API (APIv0
    or APIv1) contents can still boot correctly as remapping is done only if
    magic value (4 bytes) is found at the start of EEPROM data.
    
    Signed-off-by: Primoz Fiser <primoz.fiser@norik.com>
    Reviewed-by: Daniel Schultz <d.schultz@phytec.de>
    Signed-off-by: Wadim Egorov <w.egorov@phytec.de>

--- board/phytec/common/phytec_som_detection.h
+++ board/phytec/common/phytec_som_detection.h
@@ -37,6 +37,23 @@ static const char * const phytec_som_type_str[] = {
 	"KSP",
 };
 
+#if IS_ENABLED(CONFIG_PHYTEC_AM57_EEPROM_APIV0)
+#define PHYTEC_AM57_EEPROM_APIV0_MAGIC     0x07052017
+/* struct phytec_am57_eeprom_apiv0_data for backwards compat */
+struct phytec_am57_eeprom_apiv0_data {
+	u32 header;		/* PHYTEC EEPROM header */
+	u8 api_version;	/* EEPROM layout API version */
+	u8 mod_version;	/* PCM/PFL/PCA */
+	u8 som_pcb_rev;	/* SOM PCB revision */
+	u8 mac[6];
+	u8 ksp;		/* 1: KSP, 2: KSM */
+	u8 kspno;		/* Number for KSP/KSM module */
+	u8 kit_opt[11];	/* coding for variants */
+	u8 reserved[5];	/* not used */
+	u8 bs;		/* Bits set in previous bytes */
+} __packed;
+#endif
+
 struct phytec_api0_data {
 	u8 pcb_rev;		/* PCB revision of SoM */
 	u8 som_type;		/* SoM type */
