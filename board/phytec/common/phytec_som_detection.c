69a7d515589f612a90e8d5aa372e38340262a574
Primoz Fiser <primoz.fiser@norik.com>
2024-05-28 10:59:35 +0200

    board: phytec: common: Fix AM57x SOM detection with APIv2 EEPROM
    There should be no customer phyCORE-AM57x SOM with EEPROM APIv2 in the
    field. However some AM57x boards were flashed internally for testing and
    evaluation purposes. Since we now have phytec_am57_detect() in place,
    lets use it and make it possible to use EEPROM APIv2 on AM57x.
    
    Signed-off-by: Primoz Fiser <primoz.fiser@norik.com>
    Signed-off-by: Wadim Egorov <w.egorov@phytec.de>

--- board/phytec/common/phytec_som_detection.c
+++ board/phytec/common/phytec_som_detection.c
@@ -110,6 +110,8 @@ int phytec_eeprom_data_init_v2(struct phytec_eeprom_data *data)
 		ret = phytec_am62_detect(som, opt);
 	if (IS_ENABLED(CONFIG_PHYTEC_AM62A_SOM_DETECTION))
 		ret = phytec_am62a_detect(som, opt);
+	if (IS_ENABLED(CONFIG_PHYTEC_AM57_SOM_DETECTION))
+		ret = phytec_am57_detect(som, opt);
 	if (!ret)
 		return 0;
 
