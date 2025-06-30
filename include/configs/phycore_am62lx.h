a0970894f2b2241203f8f48665a6fb5af2e01d58
Dominik Haller <d.haller@phytec.de>
2025-06-30 15:44:40 +0200

    board: phytec: am62lx: Add PHYTEC phyCORE-AM62Lx FPSC SoM
    Add basic support for the phyCORE-AM62Lx FPSC SoM
    
    Signed-off-by: Dominik Haller <d.haller@phytec.de>
    Signed-off-by: Wadim Egorov <w.egorov@phytec.de>

---
+++ include/configs/phycore_am62lx.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Configuration header file for PHYTEC phyCORE-AM62x
+ *
+ * Copyright (C) 2025 PHYTEC Messtechnik GmbH
+ * Author: Dominik Haller <d.haller@phytec.de>
+ */
+
+#ifndef __PHYCORE_AM62LX_H
+#define __PHYCORE_AM62LX_H
+
+#include <configs/ti_armv7_common.h>
+
+#endif /* __PHYCORE_AM62LX_H */
