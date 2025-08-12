7f4ffac828e7e4fd6d335fc5e3eca7219fb7aaf7
John Ma <jma@phytec.com>
2025-08-12 12:07:43 -0700

    board: phytec: am62px: Add PHYTEC phyCORE-AM62Px SoM
    Add support for the PHYTEC phyCORE-AM62Px SoM
    
    Supported features:
    - DDR4 RAM
    - Debug UART
    - OSPI NOR Flash
    - Ethernet
    - Micro SD card
    - eMMC
    - EEPROM
    
    Signed-off-by: John Ma <jma@phytec.com>
    [Drop CONFIG_OF_BOARD_SETUP=y]
    Signed-off-by: Wadim Egorov <w.egorov@phytec.de>

---
+++ include/configs/phycore_am62px.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Configuration header file for phyCORE-AM62Px
+ *
+ * Copyright (C) 2025 PHYTEC America LLC - https://www.phytec.com/
+ */
+
+#ifndef __CONFIG_PHYCORE_AM62PX_H
+#define __CONFIG_PHYCORE_AM62PX_H
+
+#define CFG_SYS_SDRAM_BASE	0x80000000
+
+#endif /* __CONFIG_PHYCORE_AM62PX_H */
