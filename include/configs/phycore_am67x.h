c25ab06fc558e776b1598ed4de3cd63938b4cf30
Nathan Morrisson <nmorrisson@phytec.com>
2025-10-28 20:33:16 -0700

    board: phytec: am67x: Add PHYTEC phyCORE-AM67x SoM
    Add support for the PHYTEC phyCORE-AM67x SoM
    
    Supported features:
    - 4GB DDR4 RAM
    - Debug UART
    - eMMC
    - Micro SD card
    - OSPI NOR Flash
    - USB Hub
    
    Signed-off-by: Nathan Morrisson <nmorrisson@phytec.com>
    Signed-off-by: Wadim Egorov <w.egorov@phytec.de>
    [fix makefile/Kconfig because of beagleY-AI]
    Signed-off-by: Dominik Haller <d.haller@phytec.de>

---
+++ include/configs/phycore_am67x.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Configuration header file for phyCORE-AM67x SoC family
+ *
+ * Copyright (C) 2025 PHYTEC America LLC - https://www.phytec.com/
+ */
+
+#ifndef __CONFIG_PHYCORE_AM67X_H
+#define __CONFIG_PHYCORE_AM67X_H
+
+#define CFG_SYS_SDRAM_BASE	0x80000000
+
+#endif /* __CONFIG_PHYCORE_AM67X_H */
