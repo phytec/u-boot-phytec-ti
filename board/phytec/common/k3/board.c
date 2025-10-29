72f64908c36b0339806cf0b8117dc436b8bb7731
John Ma <jma@phytec.com>
2025-10-28 20:33:24 -0700

    FROMLIST: board: phytec: common: k3: Making setup_mac_from_eeprom optional
    Making the setup_mac_from_eeprom optional for boards without
    CONFIG_PHYTEC_SOM_DETECTION_BLOCKS.
    
    Signed-off-by: John Ma <jma@phytec.com>
    Reviewed-by: Wadim Egorov <w.egorov@phytec.de>
    Link: https://lists.denx.de/pipermail/u-boot/2025-August/596591.html
    Signed-off-by: Wadim Egorov <w.egorov@phytec.de>

--- board/phytec/common/k3/board.c
+++ board/phytec/common/k3/board.c
@@ -184,6 +184,7 @@ static void boot_targets_setup(void)
 	env_set("boot_targets", boot_targets);
 }
 
+#if IS_ENABLED(CONFIG_PHYTEC_SOM_DETECTION_BLOCKS)
 static void setup_mac_from_eeprom(void)
 {
 	struct phytec_api3_element *block_element;
@@ -205,13 +206,15 @@ static void setup_mac_from_eeprom(void)
 		}
 	}
 }
+#endif
 
 int board_late_init(void)
 {
 	boot_targets_setup();
 
-	if (IS_ENABLED(CONFIG_PHYTEC_SOM_DETECTION_BLOCKS))
-		setup_mac_from_eeprom();
+#if IS_ENABLED(CONFIG_PHYTEC_SOM_DETECTION_BLOCKS)
+	setup_mac_from_eeprom();
+#endif
 
 #if IS_ENABLED(CONFIG_EFI_HAVE_CAPSULE_SUPPORT)
 	configure_capsule_updates();
