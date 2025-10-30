3fd8fcfb7eb07538dbded3e14dadcf031c62cf12
Dominik Haller <d.haller@phytec.de>
2025-10-30 15:25:04 -0700

    board: phytec: am62lx: Add PHYTEC phyFLEX-AM62Lx FPSC-Gamma SoM
    Add support for the phyFLEX-AM62Lx FPSC-Gamma SoM
    
    Supported features:
    - 1-2GB DDR4 RAM
    - eMMC
    - External SD
    - 2x Ethernet
    - debug UART
    - USB DFU
    - QSPI NOR Flash (on carrier board)
    - I2C EEPROM
    
    Product page SoM: https://www.phytec.eu/en/produkte/system-on-modules/phyflex-am62lx-fpsc/
    
    Signed-off-by: Dominik Haller <d.haller@phytec.de>
    Signed-off-by: Wadim Egorov <w.egorov@phytec.de>

---
+++ board/phytec/am62lx-phyflex-fpsc-g/am62lx-phyflex-fpsc-g.c
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2025 PHYTEC Messtechnik GmbH
+ * Author: Dominik Haller <d.haller@phytec.de>
+ */
+
+#include <asm/arch/hardware.h>
+#include <asm/io.h>
+#include <dm/uclass.h>
+#include <env.h>
+#include <fdt_support.h>
+#include <spl.h>
+#include <i2c.h>
+#include <mach/k3-ddr.h>
+
+#include "../common/am6_som_detection.h"
+
+int board_init(void)
+{
+	return 0;
+}
+
+int dram_init(void)
+{
+	int ret;
+
+	ret = fdtdec_setup_mem_size_base();
+	if (ret)
+		return ret;
+
+	return k3_mem_map_init();
+}
+
+int dram_init_banksize(void)
+{
+	return fdtdec_setup_memory_banksize();
+}
+
+#if IS_ENABLED(CONFIG_XPL_BUILD)
+static u8 dram_get_size(void)
+{
+	struct phytec_eeprom_data data;
+	int ret;
+
+	ret = phytec_eeprom_data_setup(&data, CONFIG_EEPROM_BUS, EEPROM_ADDR);
+	if (!ret && data.valid)
+		return phytec_get_am6_ddr_size(&data);
+
+	/* Default DDR size is 1GB */
+	return EEPROM_RAM_SIZE_1GB;
+}
+
+void spl_perform_fixups(struct spl_image_info *spl_image)
+{
+	u64 start = CFG_SYS_SDRAM_BASE;
+	u64 size;
+	u8 ram_size;
+	int ret;
+
+	ram_size = dram_get_size();
+
+	switch (ram_size) {
+	case EEPROM_RAM_SIZE_1GB:
+		size = 0x40000000;
+		break;
+	case EEPROM_RAM_SIZE_2GB:
+		size = 0x80000000;
+		break;
+	default:
+		size = 0x40000000;
+		pr_notice("DDR size %d is not supported, using default\n", ram_size);
+		break;
+	}
+
+	ret = fdt_fixup_memory_banks(spl_image->fdt_addr, &start, &size, 1);
+	if (ret < 0)
+		pr_err("fdt_fixup_memory_banks failed (ret = %d)\n", ret);
+}
+#endif
