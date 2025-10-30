bf1e190b91e72cd3dad53452b2270c7f69df7b06
Dominik Haller <d.haller@phytec.de>
2025-10-30 15:25:04 -0700

    board: phytec: am62lx: Add PHYTEC phyFLEX-AM62Lx FPSC-Gamma SoM
    Add support for the phyFLEX-AM62Lx FPSC-Gamma SoM
    
    Supported features:
    - 1-2GB DDR4 RAM
    - eMMC
    - External SD
    - 2x Ethernet
    - debug UART
    - USB DFU
    - QSPI NOR Flash (on carrier board)
    - I2C EEPROM
    
    Product page SoM: https://www.phytec.eu/en/produkte/system-on-modules/phyflex-am62lx-fpsc/
    
    Signed-off-by: Dominik Haller <d.haller@phytec.de>
    Signed-off-by: Wadim Egorov <w.egorov@phytec.de>

---
+++ include/configs/am62lx_phyflex_fpsc_g.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Configuration header file for PHYTEC phyFLEX-AM62x
+ *
+ * Copyright (C) 2025 PHYTEC Messtechnik GmbH
+ * Author: Dominik Haller <d.haller@phytec.de>
+ */
+
+#ifndef __PHYFLEX_AM62LX_H
+#define __PHYFLEX_AM62LX_H
+
+#include <configs/ti_armv7_common.h>
+
+#endif /* __PHYFLEX_AM62LX_H */
