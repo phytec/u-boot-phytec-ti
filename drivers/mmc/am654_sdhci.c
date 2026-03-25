u-boot/u-boot

commit 2782ce5fce883554c968b8852a5ecfde0f2b9a95

mmc: am654_sdhci: Clear UHS_MODE_SELECT when <= MMC_HS_52
This clears UHS_MODE_SELECT for timing modes <= MMC_HS_52.

When initializing to HS400 mode, the host controller downgrades to non-uhs
modes so clear UHS_MODE_SELECT at modes <= MMC_HS_52.

This fixes eMMC writes on j7200 EVM.

Fixes: 6067aa66b3bb ("mmc: am654_sdhci: Add am654_sdhci_set_control_reg")
Signed-off-by: Judith Mendez <jm@ti.com>
Reviewed-by: Peng Fan <peng.fan@nxp.com>

drivers/mmc/am654_sdhci.c
@@ -527,11 +527,16 @@ static int am654_sdhci_execute_tuning(struct mmc *mmc, u8 opcode)
 void am654_sdhci_set_control_reg(struct sdhci_host *host)
 {
 	struct mmc *mmc = host->mmc;
+	u32 reg;
 
+	reg = sdhci_readw(host, SDHCI_HOST_CONTROL2);
+	reg &= ~SDHCI_CTRL_UHS_MASK;
 	sdhci_set_voltage(host);
 
 	if (mmc->selected_mode > MMC_HS_52)
 		sdhci_set_uhs_timing(host);
+	else
+		sdhci_writew(host, reg, SDHCI_HOST_CONTROL2);
 }
 
 const struct sdhci_ops am654_sdhci_ops = {

phytec/u-boot-phytec-ti

commit b79d66130fee27db4976e2817be83f2c17acb6fd

PENDING: mmc: am654_sdhci: Fix v1p8_signal_ena clobber in set_control_reg
Move Read modify write action within the else condition so
as to not clobber v1p8_signal_ena that may have been set in
sdhci_set_uhs_timing().

Fixes: 2782ce5fce88 ("mmc: am654_sdhci: Clear UHS_MODE_SELECT when <= MMC_HS_52")
Signed-off-by: Judith Mendez <jm@ti.com>

drivers/mmc/am654_sdhci.c
@@ -575,14 +575,15 @@ void am654_sdhci_set_control_reg(struct sdhci_host *host)
 	struct mmc *mmc = host->mmc;
 	u32 reg;
 
-	reg = sdhci_readw(host, SDHCI_HOST_CONTROL2);
-	reg &= ~SDHCI_CTRL_UHS_MASK;
 	sdhci_set_voltage(host);
 
-	if (mmc->selected_mode > MMC_HS_52)
+	if (mmc->selected_mode > MMC_HS_52) {
 		sdhci_set_uhs_timing(host);
-	else
+	} else {
+		reg = sdhci_readw(host, SDHCI_HOST_CONTROL2);
+		reg &= ~SDHCI_CTRL_UHS_MASK;
 		sdhci_writew(host, reg, SDHCI_HOST_CONTROL2);
+	}
 }
 
 const struct sdhci_ops am654_sdhci_ops = {