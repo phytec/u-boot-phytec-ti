phytec/u-boot-phytec-ti

commit bc08410ee8fb5235ee4720c8894aa8cdf7817428

PENDING: mach-k3: r5: common: factorize code from j721e/j784s4
lpm_process(), save_certificate() and the resume sequence are duplicated
in j721e_init.c and j784s4_init.c.

Let's factorize them and regroup other lpm-related functions in
lpm-common.c.
lpm-common.c will only be compiled for SoCs that enable CONFIG_K3_LPM.

While we're at it, wrap lines in save_certificate() that is now static
and rename lpm_process with a k3_ suffix.

Reviewed-by: Anshul Dalal <anshuld@ti.com>
Signed-off-by: Richard Genoud (TI) <richard.genoud@bootlin.com>

arch/arm/mach-k3/r5/lpm-common.c
@@ -1,0 +1,221 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * K3: R5 Common LPM Architecture initialization
+ *
+ * Copyright (C) 2023-2026 Texas Instruments Incorporated - https://www.ti.com/
+ * Copyright (C) 2026 Bootlin
+ */
+
+#include <clk.h>
+#include <dm/read.h>
+#include <elf.h>
+#include <linux/printk.h>
+#include <linux/soc/ti/ti_sci_protocol.h>
+#include <power-domain.h>
+#include <remoteproc.h>
+#include <mach/security.h>
+
+#include "../common.h"
+#include "../lpm-common.h"
+
+#define FW_IMAGE_SIZE	0x80000
+#define GTC_CNTCR_REG	0x0
+#define GTC_CNTFID0_REG	0x20
+#define GTC_CNTR_EN	0x3
+
+struct lpm_addr_info {
+	u32 *context_save_addr;
+	u32 *atf_cert_addr;
+	u32 *optee_cert_addr;
+	u32 *dm_save_addr;
+	u32 size;
+};
+
+struct lpm_addr_info mem_addr_lpm;
+
+static int extract_lpm_region(void)
+{
+	ofnode node;
+	fdt_addr_t lpm_reg_addr;
+	fdt_size_t lpm_reg_size;
+
+	node = ofnode_path("/reserved-memory/lpm-memory");
+	if (!ofnode_valid(node)) {
+		printf("lpm will not be functional\n");
+		return -ENODEV;
+	}
+
+	lpm_reg_addr = ofnode_get_addr(node);
+	if (lpm_reg_addr == FDT_ADDR_T_NONE) {
+		printf("Can't find a valid reserved node!\n");
+		return -ENODEV;
+	}
+
+	lpm_reg_size = ofnode_get_size(node);
+	if (lpm_reg_size == FDT_ADDR_T_NONE) {
+		printf("Can't find a valid reserved node!\n");
+		return -ENODEV;
+	}
+
+	mem_addr_lpm.context_save_addr = (u32 *)lpm_reg_addr;
+	mem_addr_lpm.atf_cert_addr = mem_addr_lpm.context_save_addr + FW_IMAGE_SIZE;
+	mem_addr_lpm.optee_cert_addr = mem_addr_lpm.atf_cert_addr + FW_IMAGE_SIZE;
+	mem_addr_lpm.dm_save_addr = mem_addr_lpm.optee_cert_addr + (2 * FW_IMAGE_SIZE);
+	mem_addr_lpm.size = lpm_reg_size;
+
+	return 0;
+}
+
+static void save_certificate(void)
+{
+	int ret;
+
+	if (!fit_image_info[IMAGE_ID_ATF].image_start ||
+	    !fit_image_info[IMAGE_ID_OPTEE].image_start ||
+	    !fit_image_info[IMAGE_ID_DM_FW].image_start) {
+		pr_err("Invalid images to save\n");
+		return;
+	}
+
+	ret = extract_lpm_region();
+	if (ret) {
+		pr_err("Cannot find valid LPM address range..\n");
+		return;
+	}
+
+	memcpy(mem_addr_lpm.atf_cert_addr,
+	       (void *)fit_image_info[IMAGE_ID_ATF].image_start,
+	       fit_image_info[IMAGE_ID_ATF].image_len);
+
+	memcpy(mem_addr_lpm.optee_cert_addr,
+	       (void *)fit_image_info[IMAGE_ID_OPTEE].image_start,
+	       fit_image_info[IMAGE_ID_OPTEE].image_len);
+
+	memcpy(mem_addr_lpm.dm_save_addr,
+	       (void *)fit_image_info[IMAGE_ID_DM_FW].image_start,
+	       fit_image_info[IMAGE_ID_DM_FW].image_len);
+}
+
+void k3_lpm_process(void)
+{
+	int ret = 0;
+	unsigned long save_addr;
+	struct ti_sci_handle *ti_sci = get_ti_sci_handle();
+
+	save_certificate();
+	save_addr = (unsigned long)mem_addr_lpm.context_save_addr;
+	ret = ti_sci->ops.lpm_ops.lpm_save_addr(ti_sci, save_addr,
+						mem_addr_lpm.size);
+	if (ret)
+		pr_err("TIFS lpm save addr fail\n");
+}
+
+static u32 resume_to_dm_f(void)
+{
+	struct ti_sci_handle *ti_sci = get_ti_sci_handle();
+	u32 loadaddr = 0, save_addr = 0;
+	int ret = 0;
+
+	loadaddr = (u32)mem_addr_lpm.dm_save_addr;
+	if (!valid_elf_image(loadaddr))
+		panic("%s: DM-Firmware image is not valid, it cannot be loaded\n",
+		      __func__);
+
+	loadaddr = load_elf_image_phdr(loadaddr);
+	save_addr = (uintptr_t)mem_addr_lpm.context_save_addr;
+	ret = ti_sci->ops.lpm_ops.lpm_save_addr(ti_sci, save_addr, mem_addr_lpm.size);
+	if (ret)
+		panic("TIFS lpm save addr fail : %x\n", ret);
+
+	/*
+	 * TIFS minimal context restore
+	 * This restores also the firewall
+	 */
+	ret = ti_sci->ops.lpm_ops.min_context_restore(ti_sci, 0);
+	if (ret)
+		panic("TIFS min_context_restore failed (%d)\n", ret);
+
+	/*
+	 * Restore TFA in msmc memory
+	 */
+	ret = ti_sci->ops.lpm_ops.decrypt_tfa(ti_sci,
+					      CONFIG_K3_ATF_LOAD_ADDR);
+	if (ret)
+		panic("%s: TIFS failed to decrytp TFA : %x\n", __func__, ret);
+
+	/* restore TFA resume vectore address in main core */
+	ret = ti_sci->ops.lpm_ops.core_resume(ti_sci);
+	if (ret)
+		panic("ATF failed to resume (%d)\n", ret);
+
+	return loadaddr;
+}
+
+static void resume_rproc_f(void)
+{
+	struct power_domain rproc_pwrdmn;
+	unsigned long gtc_rate;
+	struct udevice *dev;
+	struct clk gtc_clk;
+	void *gtc_base;
+	int ret;
+
+	ret = uclass_get_device_by_seq(UCLASS_REMOTEPROC, 1, &dev);
+	if (ret)
+		panic("Unknown remote processor 1 (%d)\n", ret);
+
+	ret = power_domain_get_by_index(dev, &rproc_pwrdmn, 1);
+	if (ret)
+		panic("power_domain_get_rproc() failed: %d\n", ret);
+
+	ret = clk_get_by_index(dev, 0, &gtc_clk);
+	if (ret)
+		panic("clk_get failed: %d\n", ret);
+
+	gtc_base = dev_read_addr_ptr(dev);
+	if (!gtc_base)
+		panic("Get GTC address failed\n");
+
+	gtc_rate = clk_get_rate(&gtc_clk);
+
+	/* TFA expect the Global Timebase Counter to be set-up */
+	writel((u32)gtc_rate, gtc_base + GTC_CNTFID0_REG);
+	writel(GTC_CNTR_EN, gtc_base + GTC_CNTCR_REG);
+
+	ret = power_domain_on(&rproc_pwrdmn);
+	if (ret)
+		panic("power_domain_on failed: %d\n", ret);
+}
+
+typedef void __noreturn (*image_entry_noargs_t)(void);
+
+void __noreturn k3_do_resume(void)
+{
+	image_entry_noargs_t image_entry;
+	u32 loadaddr, size_int;
+	void *image_addr;
+	int ret;
+
+	ret = extract_lpm_region();
+	if (ret)
+		panic("Cannot find valid LPM address range..LPM resume failed\n");
+
+	image_addr = (void *)mem_addr_lpm.atf_cert_addr;
+	ret = rproc_load(1, (ulong)image_addr, 0x200);
+	if (ret)
+		panic("rproc failed to be initialized (%d)\n", ret);
+
+	image_addr = mem_addr_lpm.atf_cert_addr;
+	size_int = FW_IMAGE_SIZE;
+	ti_secure_image_replay_cert(&image_addr, &size_int);
+
+	image_addr = mem_addr_lpm.optee_cert_addr;
+	ti_secure_image_replay_cert(&image_addr, &size_int);
+
+	loadaddr = resume_to_dm_f();
+	printf("Starting ATF on ARM64 core...\n\n");
+	resume_rproc_f();
+
+	image_entry = (image_entry_noargs_t)loadaddr;
+	image_entry();
+}

arch/arm/mach-k3/common.h
@@ -8,6 +8,7 @@
 
 #include <asm/armv7_mpu.h>
 #include <asm/hardware.h>
+#include <image.h>
 #include <mach/security.h>
 
 /* keep ram_top in the 32-bit address space */
@@ -15,7 +16,22 @@
 
 #define K3_FIREWALL_BACKGROUND_BIT	(8)
 #define K3_SPEED_GRADE_UNKNOWN		'\0'
-#define FW_IMAGE_SIZE 0x80000
+
+#if IS_ENABLED(CONFIG_SYS_K3_SPL_ATF)
+enum {
+	IMAGE_ID_ATF,
+	IMAGE_ID_OPTEE,
+	IMAGE_ID_SPL,
+	IMAGE_ID_DM_FW,
+	IMAGE_ID_TIFSSTUB_HS,
+	IMAGE_ID_TIFSSTUB_FS,
+	IMAGE_ID_TIFSSTUB_GP,
+	IMAGE_ID_HSM,
+	IMAGE_AMT,
+};
+
+extern struct image_info fit_image_info[IMAGE_AMT];
+#endif
 
 struct fwl_data {
 	const char *name;
@@ -42,16 +58,6 @@ enum k3_device_type {
 	K3_DEVICE_TYPE_HS_SE,
 };
 
-struct lpm_addr_info {
-	u32 *context_save_addr;
-	u32 *atf_cert_addr;
-	u32 *optee_cert_addr;
-	u32 *dm_save_addr;
-	u32 size;
-};
-
-extern struct lpm_addr_info mem_addr_lpm;
-
 void setup_k3_mpu_regions(void);
 int early_console_init(void);
 void disable_linefill_optimization(void);
@@ -71,9 +77,6 @@ struct ti_sci_handle *get_ti_sci_handle(void);
 void do_board_detect(void);
 void ti_secure_image_check_binary(void **p_image, size_t *p_size);
 int shutdown_mcu_r5_core1(void);
-void save_certificate(void);
-u32 resume_to_dm_f(void);
-void resume_rproc_f(void);
 
 #if IS_ENABLED(CONFIG_SPL_OS_BOOT_SECURE)
 int k3_falcon_fdt_fixup(void *fdt);

arch/arm/mach-k3/j721e/j721e_init.c
@@ -23,6 +23,7 @@
 
 #include "../sysfw-loader.h"
 #include "../common.h"
+#include "../lpm-common.h"
 #include <power/pmic.h>
 #include <mach/k3-ddr.h>
 
@@ -243,48 +244,6 @@ static void store_boot_info_from_rom(void)
 	       sizeof(struct rom_extended_boot_data));
 }
 
-void lpm_process(void)
-{
-	int ret = 0;
-	struct ti_sci_handle *ti_sci = get_ti_sci_handle();
-
-	save_certificate();
-	ret = ti_sci->ops.lpm_ops.lpm_save_addr(ti_sci, (uintptr_t)mem_addr_lpm.context_save_addr, mem_addr_lpm.size);
-	if (ret)
-		pr_err("TIFS lpm save addr fail\n");
-}
-
-int extract_lpm_region(void)
-{
-    ofnode node;
-	u32 lpm_reg_addr, lpm_reg_size;
-
-	node = ofnode_path("/reserved-memory/lpm-memory");
-	if (!ofnode_valid(node)) {
-		printf("lpm will not be functional\n");
-		return -ENODEV;
-	}
-
-	lpm_reg_addr = ofnode_get_addr(node);
-	if (lpm_reg_addr == FDT_ADDR_T_NONE) {
-		printf("Can't find a valid reserved node!\n");
-		return -ENODEV;
-    }
-
-	lpm_reg_size = ofnode_get_size(node);
-	if (lpm_reg_size == FDT_ADDR_T_NONE) {
-		printf("Can't find a valid reserved node!\n");
-		return -ENODEV;
-    }
-
-	mem_addr_lpm.context_save_addr = (u32 *)(uintptr_t)lpm_reg_addr;
-	mem_addr_lpm.atf_cert_addr =  (u32 *)((uintptr_t)mem_addr_lpm.context_save_addr + FW_IMAGE_SIZE);
-	mem_addr_lpm.optee_cert_addr = (u32 *)((uintptr_t)mem_addr_lpm.atf_cert_addr + FW_IMAGE_SIZE);
-	mem_addr_lpm.dm_save_addr = (u32 *)((uintptr_t)mem_addr_lpm.optee_cert_addr + (2*FW_IMAGE_SIZE));
-	mem_addr_lpm.size = lpm_reg_size;
-	return 0;
-}
-
 #ifdef CONFIG_SPL_OF_LIST
 void do_dt_magic(void)
 {
@@ -497,9 +456,6 @@ void board_init_f(ulong dummy)
 		panic("DRAM init failed: %d\n", ret);
 
 	if (j7xx_board_is_resuming()) {
-		typedef void __noreturn (*image_entry_noargs_t)(void);
-		u32 loadaddr, size_int;
-		void *image_addr;
 		/*
 		 * The DDR resume sequence is:
 		 * - exit DDR from retention
@@ -511,25 +467,8 @@ void board_init_f(ulong dummy)
 		k3_deassert_DDR_RET();
 		k3_ddrss_lpddr4_change_freq(dev);
 		k3_ddrss_lpddr4_exit_low_power(dev, &regs);
-		ret = extract_lpm_region();
-		if (ret)
-			panic("Cannot find valid LPM address range..LPM resume failed \n");
-		image_addr = (void *)mem_addr_lpm.atf_cert_addr;
-		ret = rproc_load(1, (ulong)image_addr, 0x200);
-		if (ret)
-			panic("rproc failed to be initialized (%d)\n", ret);
 
-		image_addr = mem_addr_lpm.atf_cert_addr;
-		size_int = FW_IMAGE_SIZE;
-		ti_secure_image_replay_cert(&image_addr, &size_int);
-		image_addr = mem_addr_lpm.optee_cert_addr;
-		ti_secure_image_replay_cert(&image_addr, &size_int);
-		loadaddr = resume_to_dm_f();
-		printf("Starting ATF on ARM64 core...\n\n");
-		resume_rproc_f();
-
-		image_entry_noargs_t image_entry = (image_entry_noargs_t)loadaddr;
-		image_entry();
+		k3_do_resume();
 	}
 #endif
 	spl_enable_cache();

arch/arm/mach-k3/j784s4/j784s4_init.c
@@ -22,6 +22,7 @@
 #include <mach/k3-ddr.h>
 
 #include "../sysfw-loader.h"
+#include "../lpm-common.h"
 #include "../common.h"
 
 #if IS_ENABLED(CONFIG_TARGET_J742S2_R5_EVM)
@@ -279,48 +280,6 @@ void k3_spl_init(void)
 		k3_dm_print_ver();
 }
 
-void lpm_process(void)
-{
-	int ret = 0;
-	struct ti_sci_handle *ti_sci = get_ti_sci_handle();
-	save_certificate();
-
-	ret = ti_sci->ops.lpm_ops.lpm_save_addr(ti_sci, (uintptr_t)mem_addr_lpm.context_save_addr, mem_addr_lpm.size);
-	if (ret)
-		pr_err("TIFS lpm save addr fail\n");
-}
-
-int extract_lpm_region(void)
-{
-    ofnode node;
-	u32 lpm_reg_addr, lpm_reg_size;
-
-	node = ofnode_path("/reserved-memory/lpm-memory");
-	if (!ofnode_valid(node)) {
-	    printf("lpm will not be functional\n");
-		return -ENODEV;
-	}
-
-	lpm_reg_addr = ofnode_get_addr(node);
-	if (lpm_reg_addr == FDT_ADDR_T_NONE) {
-		printf("Can't find a valid reserved node!\n");
-		return -ENODEV;
-    }
-
-	lpm_reg_size = ofnode_get_size(node);
-	if (lpm_reg_size == FDT_ADDR_T_NONE) {
-		printf("Can't find a valid reserved node!\n");
-		return -ENODEV;
-    }
-
-	mem_addr_lpm.context_save_addr = (u32 *)(uintptr_t) lpm_reg_addr;
-	mem_addr_lpm.atf_cert_addr =  (u32 *)((uintptr_t)mem_addr_lpm.context_save_addr + FW_IMAGE_SIZE);
-	mem_addr_lpm.optee_cert_addr = (u32 *)((uintptr_t)mem_addr_lpm.atf_cert_addr + FW_IMAGE_SIZE);
-	mem_addr_lpm.dm_save_addr = (u32 *)((uintptr_t)mem_addr_lpm.optee_cert_addr + (2*FW_IMAGE_SIZE));
-	mem_addr_lpm.size = lpm_reg_size;
-	return 0;
-}
-
 #define DDR_RET_VAL BIT(5)
 #define GPIO_OUT_1 0x3D
 #define PMIC_NSLEEP_REG 0x86
@@ -373,10 +332,6 @@ void k3_mem_init(void)
 		}
 
 		if (j7xx_board_is_resuming()) {
-			typedef void __noreturn (*image_entry_noargs_t)(void);
-			u32 loadaddr;
-			size_t size_int;
-			void *image_addr;
 			/* exit DDRs from retention */
 			for (ctrl = 0; ctrl < MAX_DDR_CONTROLLERS; ctrl++) {
 				k3_ddrss_lpddr4_exit_retention(devs[ctrl],
@@ -396,25 +351,8 @@ void k3_mem_init(void)
 							       &regs[ctrl]);
 			}
 			printf("Initialized %d DRAM controllers\n", ctrl);
-			ret = extract_lpm_region();
-			if (ret)
-				panic("Cannot find valid LPM address range..LPM resume failed \n");
-			image_addr = (void *)mem_addr_lpm.atf_cert_addr;
-			ret = rproc_load(1, (ulong)image_addr, 0x200);
-			if (ret)
-				panic("rproc failed to be initialized (%d)\n", ret);
 
-			image_addr = mem_addr_lpm.atf_cert_addr;
-			size_int = FW_IMAGE_SIZE;
-			ti_secure_image_replay_cert(&image_addr, &size_int);
-			image_addr = mem_addr_lpm.optee_cert_addr;
-			ti_secure_image_replay_cert(&image_addr, &size_int);
-			loadaddr = resume_to_dm_f();
-			printf("Starting ATF on ARM64 core...\n\n");
-			resume_rproc_f();
-
-			image_entry_noargs_t image_entry = (image_entry_noargs_t)(uintptr_t)loadaddr;
-			image_entry();
+			k3_do_resume();
 		}
 		printf("Initialized %d DRAM controllers\n", ctrl);
 	}

arch/arm/mach-k3/lpm-common.h
@@ -1,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * K3: LPM Architecture common definitions
+ *
+ * Copyright (C) 2026 Texas Instruments Incorporated - https://www.ti.com/
+ * Copyright (C) 2026 Bootlin
+ */
+
+#ifndef _LPM_COMMON_H_
+#define _LPM_COMMON_H_
+
+void __noreturn k3_do_resume(void);
+void k3_lpm_process(void);
+
+#endif

arch/arm/mach-k3/r5/Kconfig
@@ -1,6 +1,10 @@
 config K3_LOAD_SYSFW
 	bool
 
+config K3_LPM
+	bool
+	default y if SOC_K3_J721E || SOC_K3_J7200 || SOC_K3_J784S4
+
 config K3_OPP_LOW
 	depends on ARCH_K3 && K3_AVS0
 	bool "Enable OPP_LOW on supported TI K3 SoCs"

arch/arm/mach-k3/r5/Makefile
@@ -13,6 +13,7 @@ obj-$(CONFIG_SOC_K3_J722S) += j722s/
 obj-$(CONFIG_SOC_K3_J784S4) += j784s4/
 
 obj-y += common.o
+obj-$(CONFIG_K3_LPM) += lpm-common.o
 obj-y += lowlevel_init.o
 obj-y += r5_mpu.o
 

arch/arm/mach-k3/r5/common.c
@@ -16,25 +16,11 @@
 #include <spl.h>
 #include <remoteproc.h>
 #include <elf.h>
-#include <clk.h>
-#include <power-domain.h>
-#include <dm/read.h>
 
 #include "../common.h"
+#include "../lpm-common.h"
 
 #if IS_ENABLED(CONFIG_SYS_K3_SPL_ATF)
-enum {
-	IMAGE_ID_ATF,
-	IMAGE_ID_OPTEE,
-	IMAGE_ID_SPL,
-	IMAGE_ID_DM_FW,
-	IMAGE_ID_TIFSSTUB_HS,
-	IMAGE_ID_TIFSSTUB_FS,
-	IMAGE_ID_TIFSSTUB_GP,
-	IMAGE_ID_HSM,
-	IMAGE_AMT,
-};
-
 #if CONFIG_IS_ENABLED(FIT_IMAGE_POST_PROCESS)
 static const char *image_os_match[IMAGE_AMT] = {
 	"arm-trusted-firmware",
@@ -48,24 +34,7 @@ static const char *image_os_match[IMAGE_AMT] = {
 };
 #endif
 
-static struct image_info fit_image_info[IMAGE_AMT];
-
-struct lpm_addr_info mem_addr_lpm;
-
-__weak bool j7xx_board_is_resuming(void)
-{
-	return 0;
-}
-
-__weak void lpm_process(void)
-{
-	return;
-}
-
-__weak int extract_lpm_region(void)
-{
-	return -EINVAL;
-}
+struct image_info fit_image_info[IMAGE_AMT];
 
 void init_env(void)
 {
@@ -159,25 +128,6 @@ void release_resources_for_core_shutdown(void)
 	}
 }
 
-void save_certificate(void)
-{
-	int ret;
-
-	if (!fit_image_info[IMAGE_ID_ATF].image_start || !fit_image_info[IMAGE_ID_OPTEE].image_start || !fit_image_info[IMAGE_ID_DM_FW].image_start) {
-		pr_err("Invalid images to save\n");
-		return;
-	}
-
-	ret = extract_lpm_region();
-	if (ret)
-		pr_err("Cannot find valid LPM address range..\n");
-	else {
-		memcpy(mem_addr_lpm.atf_cert_addr, (void *)fit_image_info[IMAGE_ID_ATF].image_start, fit_image_info[IMAGE_ID_ATF].image_len);
-		memcpy(mem_addr_lpm.optee_cert_addr, (void *)fit_image_info[IMAGE_ID_OPTEE].image_start, fit_image_info[IMAGE_ID_OPTEE].image_len);
-		memcpy(mem_addr_lpm.dm_save_addr, (void *)fit_image_info[IMAGE_ID_DM_FW].image_start, fit_image_info[IMAGE_ID_DM_FW].image_len);
-	}
-}
-
 void __noreturn jump_to_image(struct spl_image_info *spl_image)
 {
 	typedef void __noreturn (*image_entry_noargs_t)(void);
@@ -225,7 +175,9 @@ void __noreturn jump_to_image(struct spl_image_info *spl_image)
 	if (ret)
 		panic("%s: ATF failed to load on rproc (%d)\n", __func__, ret);
 
-	lpm_process();
+	if (IS_ENABLED(CONFIG_K3_LPM))
+		k3_lpm_process();
+
 #if CONFIG_IS_ENABLED(FIT_IMAGE_POST_PROCESS)
 	/* Authenticate ATF */
 	void *image_addr = (void *)fit_image_info[IMAGE_ID_ATF].image_start;
@@ -308,82 +260,6 @@ void disable_linefill_optimization(void)
 	asm("mrc p15, 0, %0, c1, c0, 1" : "=r" (actlr));
 	actlr |= (1 << 13); /* Set DLFO bit  */
 	asm("mcr p15, 0, %0, c1, c0, 1" : : "r" (actlr));
-}
-
-u32 resume_to_dm_f(void)
-{
-	struct ti_sci_handle *ti_sci = get_ti_sci_handle();
-	u32 loadaddr = 0, save_addr = 0;
-	int ret = 0;
-
-	loadaddr = (u32)mem_addr_lpm.dm_save_addr;
-	if (!valid_elf_image(loadaddr))
-		panic("%s: DM-Firmware image is not valid, it cannot be loaded\n",
-		      __func__);
-	loadaddr = load_elf_image_phdr(loadaddr);
-	save_addr = (uintptr_t)mem_addr_lpm.context_save_addr;
-	ret = ti_sci->ops.lpm_ops.lpm_save_addr(ti_sci, save_addr, mem_addr_lpm.size);
-	if (ret)
-		panic("TIFS lpm save addr fail : %x\n", ret);
-	/*
-	 * TIFS minimal context restore
-	 * This restores also the firewall
-	*/
-	ret = ti_sci->ops.lpm_ops.min_context_restore(ti_sci, 0);
-	if (ret)
-		panic("TIFS min_context_restore failed (%d)\n", ret);
-	/*
-	 * Restore TFA in msmc memory
-	 */
-	ret = ti_sci->ops.lpm_ops.decrypt_tfa(ti_sci,
-					      CONFIG_K3_ATF_LOAD_ADDR);
-	if (ret)
-		panic("%s: TIFS failed to decrytp TFA : %x\n", __func__, ret);
-		/* restore TFA resume vectore address in main core */
-	ret = ti_sci->ops.lpm_ops.core_resume(ti_sci);
-	if (ret)
-		panic("ATF failed to resume (%d)\n", ret);
-
-	return loadaddr;
-}
-
-void resume_rproc_f(void)
-{
-	struct power_domain rproc_pwrdmn;
-	unsigned long gtc_rate;
-	struct udevice *dev;
-	struct clk gtc_clk;
-	void *gtc_base;
-	int ret;
-
-	ret = uclass_get_device_by_seq(UCLASS_REMOTEPROC, 1, &dev);
-	if (ret)
-		panic("Unknown remote processor 1 (%d)\n", ret);
-
-	ret = power_domain_get_by_index(dev, &rproc_pwrdmn, 1);
-	if (ret)
-		panic("power_domain_get_rproc() failed: %d\n", ret);
-
-	ret = clk_get_by_index(dev, 0, &gtc_clk);
-	if (ret)
-		panic("clk_get failed: %d\n", ret);
-
-	gtc_base = dev_read_addr_ptr(dev);
-	if (!gtc_base)
-		panic("Get GTC address failed\n");
-
-	gtc_rate = clk_get_rate(&gtc_clk);
-
-#define GTC_CNTCR_REG	0x0
-#define GTC_CNTFID0_REG	0x20
-#define GTC_CNTR_EN	0x3
-	/* TFA expect the Global Timebase Counter to be set-up */
-	writel((u32)gtc_rate, gtc_base + GTC_CNTFID0_REG);
-	writel(GTC_CNTR_EN, gtc_base + GTC_CNTCR_REG);
-
-	ret = power_domain_on(&rproc_pwrdmn);
-	if (ret)
-		panic("power_domain_on failed: %d\n", ret);
 }
 
 int remove_fwl_region(struct fwl_data *fwl)

commit 7e545c1b68fa1fc610f85f73c75bf2bb35fec084

PENDING: arm: mach-k3: r5: lpm-common: Fix pointer arithmetic in address calculation
Since context_save_addr is a u32 *, pointer arithmetic scales each offset
by sizeof(u32).

With FW_IMAGE_SIZE = 0x80000 the sub-region addresses were placed at
0x200000/0x400000/0x800000 from base instead of the intended
0x80000/0x100000/0x200000, pushing them outside the 3 MB reserved
lpm-memory region.

Cast to a u8 * before adding the byte offset.

Fixes: bc08410ee8fb ("PENDING: mach-k3: r5: common: factorize code from j721e/j784s4")

Signed-off-by: Abhash Kumar Jha <a-kumar2@ti.com>
Reviewed-by: Manorit Chawdhry <m-chawdhry@ti.com>

arch/arm/mach-k3/r5/lpm-common.c
@@ -58,9 +58,12 @@ static int extract_lpm_region(void)
 	}
 
 	mem_addr_lpm.context_save_addr = (u32 *)lpm_reg_addr;
-	mem_addr_lpm.atf_cert_addr = mem_addr_lpm.context_save_addr + FW_IMAGE_SIZE;
-	mem_addr_lpm.optee_cert_addr = mem_addr_lpm.atf_cert_addr + FW_IMAGE_SIZE;
-	mem_addr_lpm.dm_save_addr = mem_addr_lpm.optee_cert_addr + (2 * FW_IMAGE_SIZE);
+	mem_addr_lpm.atf_cert_addr =
+		(u32 *)((u8 *)mem_addr_lpm.context_save_addr + FW_IMAGE_SIZE);
+	mem_addr_lpm.optee_cert_addr =
+		(u32 *)((u8 *)mem_addr_lpm.atf_cert_addr + FW_IMAGE_SIZE);
+	mem_addr_lpm.dm_save_addr =
+		(u32 *)((u8 *)mem_addr_lpm.optee_cert_addr + (2 * FW_IMAGE_SIZE));
 	mem_addr_lpm.size = lpm_reg_size;
 
 	return 0;