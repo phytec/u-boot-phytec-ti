6025598b81af193c13ac7aa40dfdbe00c3b2fad3
Randolph Sapp <rs@ti.com>
2026-07-08 14:28:49 -0500

    PENDING: beaglebadge: add efi capsule support
    Considering a copy of the boot-loader binaries will be stored on OSPI
    out of the factory, using capsules to update these binaries would be
    preferable.
    
    Add a new k3-am62l3-evm-capsule.dtsi to keep a copy of the relevant
    capsule nodes, similar to the K3 a53 and r5 capsule dtsi files
    currently. Going for duplication instead of direct inclusion currently
    to avoid any issues in the future and follow the pattern set with the
    current k3-am62l3-evm-binman.dtsi.
    
    Despite mtd being used behind the scenes for the sf command, using the
    mtd command directly in the EFI dfu_string results in a failure. Seems
    that the device isn't being probed correctly in that path.
    
    Signed-off-by: Randolph Sapp <rs@ti.com>

--- board/beagle/beaglebadge/beaglebadge.c
+++ board/beagle/beaglebadge/beaglebadge.c
@@ -6,9 +6,36 @@
  *
  */
 
+#include <config.h>
+#include <efi_loader.h>
 #include <env.h>
 #include <fdtdec.h>
 #include <fdt_support.h>
+
+struct efi_fw_image fw_images[] = {
+	{
+		.image_type_id = BEAGLEBADGE_TIBOOT3_IMAGE_GUID,
+		.fw_name = u"BEAGLEBADGE_TIBOOT3",
+		.image_index = 1,
+	},
+	{
+		.image_type_id = BEAGLEBADGE_SPL_IMAGE_GUID,
+		.fw_name = u"BEAGLEBADGE_SPL",
+		.image_index = 2,
+	},
+	{
+		.image_type_id = BEAGLEBADGE_UBOOT_IMAGE_GUID,
+		.fw_name = u"BEAGLEBADGE_UBOOT",
+		.image_index = 3,
+	}
+};
+
+struct efi_capsule_update_info update_info = {
+	.dfu_string = "sf 0:0=tiboot3.bin part 0 1;tispl.bin part 0 2;"
+		      "u-boot.img part 0 3",
+	.num_images = ARRAY_SIZE(fw_images),
+	.images = fw_images,
+};
 
 int board_init(void)
 {
--- include/configs/beaglebadge.h
+++ include/configs/beaglebadge.h
@@ -14,4 +14,29 @@
 
 #define CFG_SYS_UBOOT_BASE CONFIG_TEXT_BASE
 
+/**
+ * define BEAGLEBADGE_TIBOOT3_IMAGE_GUID - firmware GUID for BeagleBadge
+ *                                         tiboot3.bin
+ * define BEAGLEBADGE_SPL_IMAGE_GUID     - firmware GUID for BeagleBadge SPL
+ * define BEAGLEBADGE_UBOOT_IMAGE_GUID   - firmware GUID for BeagleBadge UBOOT
+ *
+ * These GUIDs are used in capsules updates to identify the corresponding
+ * firmware object.
+ *
+ * Board developers using this as a starting reference should
+ * define their own GUIDs to ensure that firmware repositories (like
+ * LVFS) do not confuse them. Generated with "mkeficapsule guidgen".
+ */
+#define BEAGLEBADGE_TIBOOT3_IMAGE_GUID \
+	EFI_GUID(0x0F498FDE, 0xD6A5, 0x53F6, 0xA9, 0x87, \
+		0x34, 0xB1, 0x83, 0x67, 0xF8, 0x7F)
+
+#define BEAGLEBADGE_SPL_IMAGE_GUID \
+	EFI_GUID(0x0C989265, 0x2034, 0x54A9, 0xA2, 0xCB, \
+		0x10, 0xCF, 0x4A, 0xD6, 0x21, 0xB9)
+
+#define BEAGLEBADGE_UBOOT_IMAGE_GUID \
+	EFI_GUID(0x783F68D7, 0x06A4, 0x5AEC, 0x83, 0xAA, \
+		0xFA, 0xA1, 0xA8, 0x10, 0xEA, 0x88)
+
 #endif /* __CONFIG_BEAGLEBADGE_H */
